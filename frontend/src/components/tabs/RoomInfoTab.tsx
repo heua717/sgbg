1c3d285e22babd9ad60be7244490c88e45b9e9d3
jimin <jimin4661@gmail.com>
2022-09-20 17:47:25 +0900

    [S07P22A707-127] feat: room info tab markup

--- frontend/src/components/tabs/RoomInfoTab.tsx
+++ frontend/src/components/tabs/RoomInfoTab.tsx
@@ -4,17 +4,15 @@ const RoomInfoTabs = () => {
   return (
     <div>
       <h3 className='font-semibold'>[이색 놀거리] 블루 하와이안 만들기</h3>
-      <hr className="my-2" />
+      <hr className="mt-5 mb-2" />
 
-      <div className="grid grid-cols-6">
-      <p className="col-span-2">모집인원</p>
+      <div className="text-sm flex justify-between">
+      <p className="flex">모집 인원</p>
       {/* grid grid-cols-3 */}
-      <div className="col-span-4 grid grid-cols-5">
-        <input type="number" id="minPerson" className="" />
-        <label htmlFor="minPerson" className="text-center">명</label>
+      <div className="grid grid-cols-3">
+        <span>2 명</span>
         <span className="text-center">~</span>
-        <input type="number" id="maxPerson" className="" />
-        <label htmlFor="maxPerson" className="text-center">명 </label>
+        <span>5 명</span>
       </div>
     </div>
     <hr className="my-2" />
