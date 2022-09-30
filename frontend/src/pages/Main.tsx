b33dbf6f2142a4bd85aa6cebc65857f7c1c23984
jimin <jimin4661@gmail.com>
2022-09-30 13:30:39 +0900

    [S07P22A707-197] chore: main-infinite-scroll-ongoing

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -14,31 +14,26 @@ import { roomMore } from "../util/room";
 const Main = () => {
   // axios 통신 0929 임지민
   const [mainRoomList, setMainRoomList] = useState<roomMore[]>([])
-
-  // 인피니트 스크롤 0930 임지민
-  // 필요한 변수 설정
-  const [ page, setPage ] = useState(1)
   const [ isFetching, setFetching ] = useState(false)
-  const [ hasNextPage, setNextPage ] = useState(true)
+  const [ page, setPage ] = useState(1)
 
-  // 인피니트 스크롤 메인 함수
-  useEffect(()=> {
+  const fetchMainRoomList = async () => {
+    setFetching(true)
 
-    getRoomList(page, 5, 'roomId,DESC')
-    .then((res) =>{
-      // const list:object = res.data.roomListRes
-      // console.log(list, typeof(list), typeof(mainRoomList));
-      console.log(res.data);
+    await getRoomList(page, 5, 'roomId, DESC')
+    .then((res) => {
+      console.log(res.data.roomListRes);
+      // setMainRoomList(mainRoomList.concat(res.data.))
       
-      setMainRoomList(mainRoomList.concat(res.data.roomListRes))
-      setPage(page+1)
-      setFetching(false)
-    });
-    // console.log('mainroomlist', mainRoomList, typeof(mainRoomList));
+    })
+  }
   
-  }, [page])
 
   // 화면에 띄우기 위한 임시 리스트
+  useEffect(()=>{
+    console.log(isFetching);
+    
+  })
   return (
     <div>
       {/* 로고 */}
@@ -48,14 +43,16 @@ const Main = () => {
       {/* 필터바 */}
       <FilterBar />
       {/* 모임리스트 */}
-      <div className="w-per95 m-auto grid grid-cols-1 gap-1">
-        {mainRoomList.map((room:roomMore) => (
-          <Link to={`/meeting/${room.roomId}`}>
-            {/* <p>{room.title}</p> */}
-            <MeetingCard name="main" room={room}></MeetingCard>
-          </Link>
-        ))}
-      </div>
+      {isFetching && (
+        <div className="w-per95 m-auto grid grid-cols-1 gap-1">
+          {mainRoomList.map((room:roomMore) => (
+            <Link to={`/meeting/${room.roomId}`}>
+              {/* <p>{room.title}</p> */}
+              <MeetingCard name="main" room={room}></MeetingCard>
+            </Link>
+          ))}
+        </div>
+      )}
 
       {/* 모임버튼 */}
       <div>
