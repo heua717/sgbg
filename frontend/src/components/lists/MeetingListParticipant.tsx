639706e6fb2b083d9c3923e7ee4fb4ba1eb17c0e
NamjuPark-dev <dev.nammjuicy@gmail.com>
2022-10-01 19:47:09 +0900

    fix:fix mypage axios

--- frontend/src/components/lists/MeetingListParticipant.tsx
+++ frontend/src/components/lists/MeetingListParticipant.tsx
@@ -3,48 +3,55 @@ import { useEffect, useState } from "react";
 import { getMyPageParticipantList } from "../../api/profile";
 
 type room = {
-    roomId: number,
-    hostId: number,
-    hostName: string,
-    parentCategory: string,
-    childCategory: string,
-    title: string,
-    maxUser: number,
-    minUser: number,
-    price: number,
-    location: string,
-    reservationDate: string,
-    endDate: string,
-    minMemberScore: number,
-    description: string,
-    members: [
-      {
-        kakaoId: string,
-        name: string,
-        email: string,
-        hostScore: number,
-        memberScore: number
-      }
-    ]
-  }
+  roomId: number;
+  hostId: number;
+  hostName: string;
+  parentCategory: string;
+  childCategory: string;
+  title: string;
+  maxUser: number;
+  minUser: number;
+  price: number;
+  location: {
+    locationId: string;
+    name: string;
+    latitude: string;
+    hardness: string;
+    roadAddress: string;
+  };
+  reservationDate: string;
+  endDate: string;
+  minMemberScore: number;
+  description: string;
+  members: [
+    {
+      kakaoId: string;
+      name: string;
+      email: string;
+      hostScore: number;
+      memberScore: number;
+    }
+  ];
+};
 
 const MeetingListParticipant = () => {
-    const [roomList, setRoomList] = useState<room[]>([]);
-    useEffect(() => {
-        getMyPageParticipantList().then(({ data }) => {
-            if (data.statusCode !== 2000) {
-                throw Error();
-            }
-            setRoomList([...data.roomListInfo]);
-        }).catch();
-    });
-    return (
-        <div className="w-full">
-            {roomList.map((room) => 
-                <MeetingCard key={ room.roomId } name="meetingListParticipant" room={room} />)
-            }
-        </div>
-    );
+  const [roomList, setRoomList] = useState<room[]>([]);
+  useEffect(() => {
+    getMyPageParticipantList()
+        .then(({ data }) => {
+          console.log(data);
+          
+        setRoomList([...data.roomListInfo]);
+      })
+      .catch();
+  });
+  return (
+    <div className="w-full">
+      {roomList.map((room) => (
+        <MeetingCard key={room.roomId} name="meetingListParticipant" room={room} />
+      ))}
+    </div>
+  );
 };
 
 export default MeetingListParticipant;
