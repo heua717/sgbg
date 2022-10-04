3792b9a0d119d250ca806e237b31afafcf34a8f8
jimin <jimin4661@gmail.com>
2022-10-04 17:21:21 +0900

    [S07P22A707-264] search-api

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,4 +1,4 @@
-export const defaultURL = "http://j7a707.p.ssafy.io/api/";
-export const mapURL = "https://dapi.kakao.com/v2/local/search";
-export const kakaoRedirect = "http://j7a707.p.ssafy.io/login";
+export const defaultURL = "https://j7a707.p.ssafy.io/api/";
+export const mapURL = "http://dapi.kakao.com/v2/local/search";
+export const kakaoRedirect = "https://j7a707.p.ssafy.io/login";
 
