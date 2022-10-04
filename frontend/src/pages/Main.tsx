ad8c6071f07c423e5c3d58cc3861265cd4ee7c5f
NamjuPark-dev <dev.nammjuicy@gmail.com>
2022-10-04 21:06:45 +0900

    [S07P22A707-234]feat:add member evaluation

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,11 +1,10 @@
 import CategoriesBar from "../components/bars/CategoriesBar";
-import FilterBar from "../components/bars/FilterBar";
 import BtnCreateRoom from "../components/buttons/BtnCreateRoom";
 import MeetingCard from "../components/cards/MeetingCard";
 import Logo from "../components/etc/Logo";
 // import MeetingReviewModal from "../components/modals/MeetingReviewModal";
 import { Link } from "react-router-dom";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getRoomList } from "../api/main";
 import { roomMore } from "../util/room";
 
@@ -13,24 +12,22 @@ import { roomMore } from "../util/room";
 
 const Main = () => {
   // axios 통신 0929 임지민
-  const [mainRoomList, setMainRoomList] = useState<roomMore[]>([])
+  const [mainRoomList, setMainRoomList] = useState<roomMore[]>([]);
 
   const fetchMainRoomList = async () => {
     // setFetching(true)
 
-    await getRoomList('roomId, DESC')
-    .then(({data}) => {
+    await getRoomList("roomId, DESC").then(({ data }) => {
       console.log(data.roomListInfo);
-      setMainRoomList(mainRoomList.concat(data.roomListInfo))
-    }, )
-  }
-  
+      setMainRoomList(mainRoomList.concat(data.roomListInfo));
+    });
+  };
 
   // 화면에 띄우기 위한 임시 리스트
-  useEffect(()=>{
+  useEffect(() => {
     // console.log(isFetching);
-    fetchMainRoomList()
-  }, [])
+    fetchMainRoomList();
+  }, []);
 
   return (
     <div>
@@ -42,7 +39,7 @@ const Main = () => {
       {/* <FilterBar /> */}
       {/* 모임리스트 */}
       <div className="w-per95 m-auto grid grid-cols-1 gap-1">
-        {mainRoomList.map((room:roomMore) => (
+        {mainRoomList.map((room: roomMore) => (
           <Link to={`/meeting/${room.roomId}`} key={room.roomId}>
             {/* <p>{room.title}</p> */}
             <MeetingCard name="main" room={room}></MeetingCard>
