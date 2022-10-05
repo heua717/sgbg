6f2933f41e5ec36818bb6c5456f9906e38f28076
jimin <jimin4661@gmail.com>
2022-10-06 00:39:08 +0900

    chore:push for test - read room component 렌더링할 때 members가 state에 제대로 저장이 안됨

--- frontend/src/pages/room/ReadRoom.tsx
+++ frontend/src/pages/room/ReadRoom.tsx
@@ -1,6 +1,7 @@
 import MeetingCard from "../../components/cards/MeetingCard";
 import Logo from "../../components/etc/Logo";
 import RoomTabs from "../../components/tabs/RoomTabs";
+import BtnAddOrDelete from "../../components/buttons/BtnAddOrDelete";
 import { roomMore } from "../../util/room";
 import { useEffect, useState } from "react";
 import { readRoom, intoRoom, outRoom } from "../../api/room";
@@ -48,8 +49,10 @@ const ReadRoom = () => {
   // 현재 유저가 이 모임 참여자 목록에 있는 지 없는 지 판단
   const [isInThisRoom, setIsInThisRoom] = useState(false);
 
-  const getIsInThisRoom = () => {
-    room.members.forEach((member: members) => {
+  const getIsInThisRoom = (members:members[]) => {
+    console.log('members= ', members);
+    
+    members.forEach((member: members) => {
       console.log(member);
       
       console.log('readroom kakaoid', member.kakaoId, 'readroom current user=', userAuth.userId);
@@ -62,20 +65,6 @@ const ReadRoom = () => {
     });
   };
 
-  useEffect(() => {
-    if (meeting_id) {
-      readRoom(meeting_id)
-        .then(({ data }) => {
-          setRoom({ ...data.roomInfo });
-        })
-        .catch((e) => {});
-    }
-
-    getIsInThisRoom();
-    // console.log('readroommembers=',  room.members);
-  }, []);
-
-  const navigate = useNavigate();
 
   const onClickInAndOut = () => {
     // 참여자 늘리는 axios 요청 보내기
@@ -156,12 +145,107 @@ const ReadRoom = () => {
     }
   };
 
+
+  useEffect(() => {
+    if (meeting_id) {
+      readRoom(meeting_id)
+        .then(({ data }) => {
+          setRoom({ ...data.roomInfo });
+          getIsInThisRoom(data.roomInfo.members);
+        })
+        .catch((e) => {});
+    }
+    console.log('readroommembers=',  room.members);
+  }, []);
+
+  const navigate = useNavigate();
+
+  // const onClickInAndOut = () => {
+  //   // 참여자 늘리는 axios 요청 보내기
+  //   // 로그인이 안되어 있으면 로그인 페이지로 넘기기
+  //   // 로그인이 되어 있으면 axios 요청
+  //   // 로그인 막기
+  //   if (!userAuth.isLogined) {
+  //     Swal.fire({
+  //       position: "center",
+  //       icon: "error",
+  //       title: "로그인 후에 모임 참여가 가능합니다.",
+  //       showConfirmButton: false,
+  //       timer: 1500,
+  //     }).then(() => {
+  //       navigate("/login");
+  //     });
+  //   } // 퇴징: 로그인은 되어 있지만 이미 참여하고 있는 경우
+  //   else if (userAuth.isLogined && isInThisRoom) {
+  //     Swal.fire({
+  //       position: "center",
+  //       icon: "info",
+  //       title: "이미 참여하고 있는 모임입니다. 퇴장하시겠습니까?",
+  //       showConfirmButton: true,
+  //       showCancelButton: true,
+  //       cancelButtonText: "취소",
+  //       timer: 1500,
+  //     }).then(() => {
+  //       //axios로 퇴장 시키기
+  //       try {
+  //         setLogining(true);
+  //         outRoom(room.roomId).then(() => {
+  //           setLogining(false);
+  //           Swal.fire({
+  //             position: "center",
+  //             title: `${room.title}에서 퇴장하셨습니다.`,
+  //             timer: 1500,
+  //           }).then(() => navigate(0));
+  //         });
+  //       } catch {
+  //         setLogining(false);
+  //         Swal.fire({
+  //           toast: true,
+  //           position: "center",
+  //           showConfirmButton: true,
+  //           title: `입장 실패하였습니다.`,
+  //         });
+  //       }
+  //     });
+  //   }
+  //   // 입장: 로그인이 되어 있는 데 현재 방에 참가하고 있지 않은 경우
+  //   else if (userAuth.isLogined && !isInThisRoom) {
+  //     if (room.roomId) {
+  //       Swal.fire({
+  //         title: `${room.title}에 참여하시겠습니까?`,
+  //         showCancelButton: true,
+  //         confirmButtonText: "참여하기",
+  //         cancelButtonText: "취소",
+  //       }).then((result) => {
+  //         if (result.isConfirmed) {
+  //           try {
+  //             setLogining(true);
+  //             intoRoom(room.roomId).then(() => {
+  //               setLogining(false);
+  //               navigate(0);
+  //             });
+  //           } catch {
+  //             setLogining(false);
+  //             Swal.fire({
+  //               toast: true,
+  //               position: "center",
+  //               showConfirmButton: true,
+  //               title: `퇴장 실패하였습니다.`,
+  //             });
+  //           }
+  //         }
+  //       });
+  //     }
+  //   }
+  // };
+
   return (
     <div>
       <div>
         <Logo />
         <div className="px-3">
           <MeetingCard name="readRoom" room={room} />
+          {/* <BtnAddOrDelete room={room}/> */}
           {islogining ? (
             <div className="w-full flex flex-col justify-center items-center">
               <div className="flex flex-row">
