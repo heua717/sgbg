58a655e5476f422ba5e2111d2163d3a31c4c3088
jimin <jimin4661@gmail.com>
2022-10-06 10:06:28 +0900

    chore: push for test

--- frontend/src/components/etc/Logo.tsx
+++ frontend/src/components/etc/Logo.tsx
@@ -12,7 +12,7 @@ const Logo = () => {
     const url = window.location.href
     // console.log(url);
     //url === 'http://localhost:3000/' || 
-    if (url === 'https://j7a707.p.ssafy.io/') {
+    if (url === 'http://localhost:3000/' || url === 'https://j7a707.p.ssafy.io/') {
       console.log('open drawer');
       !isOpen? setIsOpen(true) : setIsOpen(false)
     } else {
